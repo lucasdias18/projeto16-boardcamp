a7691f3b419d3c5654858bbeb37216ff1a448da9
Lucas Dias <lucasdias.ld10@gmail.com>
2023-02-13 17:55:31 -0300

    pequenas correções em clientes e jogos'

--- src/schema/AuthSchema.js
+++ src/schema/AuthSchema.js
@@ -3,6 +3,6 @@ import joi from 'joi'
 export const userSchema = joi.object({
   name: joi.string().required(),
   phone: joi.string().required(),
-  cpf: joi.string().required().min(11).max(11),
+  cpf: joi.string().alphanum().required().min(11).max(11),
   birthday: joi.string().required()
 });
